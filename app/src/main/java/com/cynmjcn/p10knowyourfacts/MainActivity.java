ce7b6b95b0de432f32d49bdd55466f1e2768a63c
acidjive <16022578@myrp.edu.sg>
2019-07-16 17:28:23 +0800

    Initial commit

---
+++ app/src/main/java/com/cynmjcn/p10knowyourfacts/MainActivity.java
@@ -0,0 +1,116 @@
+package com.cynmjcn.p10knowyourfacts;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+    Button btnReadLater;
+    AlarmManager am;
+    int reqCode = 123;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("current", vPager.getCurrentItem());
+        editor.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        int current = prefs.getInt("current", 0);
+
+        vPager.setCurrentItem(current,true);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vPager = (ViewPager) findViewById(R.id.viewpager1);
+        btnReadLater = (Button) findViewById(R.id.btnReadLater);
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+        al.add(new Frag3());
+
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+
+        btnReadLater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.MINUTE, 5);
+                Intent intent = new Intent(MainActivity.this, ScheduledNotificationReceiver.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this,reqCode, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+                am = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+                finish();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_previous:
+                if(vPager.getCurrentItem() > 0) {
+                    int previousPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previousPage, true);
+                }
+                return true;
+
+            case R.id.action_random:
+
+                Random random = new Random();
+                vPager.setCurrentItem(random.nextInt(4), true);
+
+                return true;
+
+            case R.id.action_next:
+                int max = vPager.getChildCount();
+                if(vPager.getCurrentItem() < max-1) {
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+            default:
+                return super.onOptionsItemSelected(item);
+        }    }
+}
